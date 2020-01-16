71dcf67d4971d56aaaf9e584378a4c8d20883880
raed.abusanad <raed.abusanad@hotmail.com>
2020-01-16 19:36:00 +0000

    header

--- src/App.js
+++ src/App.js
@@ -44,28 +44,36 @@ class App extends Component {
     return (
       <div className="App">
 
-        <div style={{ backgroundColor: '#3D94F6', minHeight: '50h', padding: '20px' }}>
-          <h3>This is a single component app.</h3>
-        </div>
+        <header className='App-header'>
+
+          <p>
+            What makes a react component to update is a change in its state or props.<br />
+            Update involves calling multiple life cycle events and one of them is <b>render()</b> method, which is what we are intersted in.<br />
+            <a href='https://reactjs.org/docs/react-component.html#updating'>Link</a>
+            <br /><br />
+            When a component is connected to redux 2 things must be satisfied for a component to rerender:<br /><br />
+            1. A change to store state, which will lead to re-evaluation of mapStateToProps.<br />
+            2. A change to at least one of the mapStateToProps properties used by the component.
+          </p>
+
+          <p>Note: We are using a single react component. No children react components.</p>
+
+        </header>
 
         <div style={{ margin: '50px' }}>
-          <button className='mybutton' onClick={this.incrementCounterAction}>Increment Counter</button>
-          <button className='mybutton' onClick={this.resetCounterAction}>Reset Counter</button>
-          <button className='mybutton' onClick={this.unknownAction}>Unknown Action</button>
-          <button className='mybutton' onClick={this.extraCounterAction}>Extra Counter</button>
+          <button className='mybutton' onClick={this.incrementCounterAction}>1. Increment Counter</button>
+          <button className='mybutton' onClick={this.resetCounterAction}>2. Reset Counter</button>
+          <button className='mybutton' onClick={this.unknownAction}>3. Unknown Action</button>
+          <button className='mybutton' onClick={this.extraCounterAction}>4. Extra Counter</button>
         </div>
-{/* 
-        <div className='center'>
-          <p>this.</p>
-        </div> */}
 
         <div style={{ marginTop: '50px', backgroundColor: '#3D94F6', minHeight: '100vh', padding: '20px' }}>
 
-          <h2>Component render count:</h2>
-          <p className='App-p'>{this.renderCount}</p>
+          <p className='label'>Component render count:</p>
+          <p className='label'>{this.renderCount}</p>
 
-          <h2>Counter state prop:</h2>
-          <p className='App-p'>{this.props.count}</p>
+          <p className='label'>Counter state prop:</p>
+          <p className='label'>{this.props.count}</p>
 
         </div>
 
